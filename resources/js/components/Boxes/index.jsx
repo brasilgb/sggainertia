508ca7bdc36fce54b8eb24c45d0c138319808b37
Anderson Brasil <andersonbrasil72@gmail.com>
2022-01-20 21:31:23 -0300

    Push

---
+++ resources/js/components/Boxes/index.jsx
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+
+export const BoxContainer = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+  };
+  
+  export const BoxHeader = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+  };
+  
+  export const BoxBody = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+  };
+
+  export const BoxFooter = () => {
+      return (
+          <Fragment>
+              
+          </Fragment>
+      );
+  }
+
+  
+          
